04ffad79a73a82a58115d95f6ad2825ab9b72f67
Amelie Barbosa <amelieelibh@gmail.com>
2017-05-19 20:22:56 +0000

    folio upgrade details

--- lib/folio.dart
+++ lib/folio.dart
@@ -1,19 +1,36 @@
 class Folio{
   String folio;
-  String nombreProyecto;
   String idResponsable;
-  
+  String cc;
+  String centroCostos;
+  String descripcion;
+  double monto;
+  bool check;
 
-  Folio(this.folio, this.nombreProyecto, this.idResponsable);
+  Folio({String folio : "", String idResponsable : "", String cc : "",  
+      String centroCostos : "", String descripcion : "", double monto : 0.00, 
+      bool check : false}){
+    this.folio = folio;
+    this.idResponsable = idResponsable;
+    this.cc = cc;
+    this.centroCostos = centroCostos;
+    this.descripcion = descripcion;
+    this.monto = monto;
+    this.check = check;
+  }
   
   @override
   String toString(){
     return "folio:" + folio 
-    + " nombreProyecto:" + nombreProyecto
-    + " idResponsable:" + idResponsable;
+    + " idResponsable:" + idResponsable
+    + " cc:" + cc
+    + " centroCostos:" + centroCostos
+    + " descripcion:" + descripcion
+    + " monto:" + monto
+    + " check:" + check;
   }
 
   static Folio EMPTY(){
-    return new Folio("", "", "");
+    return new Folio();
   }
 }
--- lib/folios_service.dart
+++ lib/folios_service.dart
@@ -6,14 +6,13 @@ import 'dart:async';
 @Injectable()
 class FoliosService {
   final List<Folio> folios = [
-    new Folio("00001", "Novela Personajes", "A0001"),
-    new Folio("00020", "Comercial OneShot", "A0001"),
-    new Folio("00300", "Reportaje de la 1", "A0001"),
-    new Folio("04000", "Reportaje de las 8", "A0001"),
-    new Folio("50000", "Entrevista ciudadana", "A0001"),
-    new Folio("60701", "Tour de la Academia", "A0001"),
-    new Folio("89015", "La Academia", "A1000"),
-    new Folio("99990", "Publicidad", "A10000")
+    new Folio(folio:"00001", cc:"02275", idResponsable:"A0001", centroCostos:"Lo que callaos las mujeres", descripcion:"Viaje Acapulco", monto:5000.00, check:true),
+    new Folio(folio:"00020", cc:"31402", idResponsable:"A0001", centroCostos:"Lo que callaos las mujeres", descripcion:"Opening", monto:45000.00, check:true),
+    new Folio(folio:"04000", cc:"85420", idResponsable:"A0001", centroCostos:"La Fiscal de Hierro", descripcion:"Promocionales", monto:6500.00, check:true),
+    new Folio(folio:"50001", cc:"31402", idResponsable:"A0001", centroCostos:"La Fiscal de Hierro", descripcion:"Opening", monto:55000.00, check:true),
+    new Folio(folio:"60701", cc:"85420", idResponsable:"A0001", centroCostos:"Nada Personal", descripcion:"Promocionales", monto:8000.00, check:true),
+    new Folio(folio:"89015", cc:"02275", idResponsable:"A0001", centroCostos:"Nada Personal", descripcion:"Viaje Queretaro", monto:15000.00, check:true),
+    new Folio(folio:"99990", cc:"55055", idResponsable:"A0001", centroCostos:"La Academia", descripcion:"Publicidad", monto:29000.50, check:true),
   ];
 
   Future<List<Folio>> getFolios(String userid) async{
