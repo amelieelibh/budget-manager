f88e848836ff1265b51ba91641fcbb669127ee70
Amelie Barbosa <amelieelibh@gmail.com>
2017-06-01 01:01:34 +0000

    preparacion del proyecto para despliegue en appengine

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,1 @@
-# The standard nginx container just runs nginx. The configuration file added
-# below will be used by nginx.
 FROM google/dart-runtime
--- bin/server.dart
+++ bin/server.dart
@@ -1,66 +1,65 @@
-// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+library budgetserver;
 
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:appengine/appengine.dart';
-import 'package:gcloud/db.dart';
+import 'package:http_server/http_server.dart';
+import 'package:logging/logging.dart';
+import 'package:rpc/rpc.dart';
 
-import 'package:clientserver/model.dart';
+//import 'package:server_code_lab/server/piratesapi.dart';
 
-void main() {
-  runAppEngine(requestHandler);
+//final ApiServer _apiServer = new ApiServer(prettyPrint: true);
+
+// Create a virtual directory used to serve our client code from
+// the 'build/web' directory.
+final String _buildPath =
+    Platform.script.resolve('../build/web/').toFilePath();
+final VirtualDirectory _clientDir = new VirtualDirectory(_buildPath);
+
+Future main() async {
+  // Add a bit of logging...
+  Logger.root..level = Level.INFO
+             ..onRecord.listen(print);
+
+  // Set up a server serving the pirate API.
+  /*
+  _apiServer.addApi(new PiratesApi());
+  */
+  HttpServer server =
+      await HttpServer.bind(InternetAddress.ANY_IP_V4, 8082);
+  server.listen(requestHandler);
+  print('Server listening on http://${server.address.host}:'
+        '${server.port}');
 }
 
 Future requestHandler(HttpRequest request) async {
-  if (request.uri.path == '/items') {
-    handleItems(request);
-  } else if (request.uri.path == '/clean') {
-    await handleClean(request);
-    await request.response.redirect(Uri.parse('/index.html'));
-  } else if (request.uri.path == '/') {
-    await request.response.redirect(Uri.parse('/index.html'));
-  } else {
-    await context.assets.serve();
-  }
-}
-
-Future handleItems(HttpRequest request) async {
-  if (request.method == 'GET') {
-    final List<Item> items = await queryItems();
-    final result = items.map((item) => item.serialize()).toList();
-    final json = {'success': true, 'result': result};
-    await sendJSONResponse(request, json);
-  } else if (request.method == 'POST') {
-    final json = await readJSONRequest(request);
-    final item = Item.deserialize(json)..parentKey = itemsRoot;
-    final error = item.validate();
-    if (error != null) {
-      await sendJSONResponse(request, {'success': false, 'error': error});
-    } else {
-      await dbService.commit(inserts: [item]);
-      await sendJSONResponse(request, {'success': true});
+  if (request.uri.path.startsWith('/budget')) {
+    // Handle the API request.
+    //var apiResponse;
+    try {
+      //var apiRequest = new HttpApiRequest.fromHttpRequest(request);
+      //apiResponse =
+      //    await _apiServer.handleHttpApiRequest(apiRequest);
+    } catch (error, stack) {
+      /*var exception =
+          error is Error ? new Exception(error.toString()) : error;
+      apiResponse = new HttpApiResponse.error(
+          HttpStatus.INTERNAL_SERVER_ERROR, exception.toString(),
+          exception, stack);
+      */
     }
+    return sendApiResponse(apiResponse, request.response);
+  } else if (request.uri.path == '/') {
+    // Redirect to the piratebadge.html file. This will initiate
+    // loading the client application.
+    request.response.redirect(Uri.parse('/index.html'));
+  } else {
+    // Serve the requested file (path) from the virtual directory,
+    // minus the preceeding '/'. This will fail with a 404 Not Found
+    // if the request is not for a valid file.
+    var fileUri = new Uri.file(_buildPath)
+        .resolve(request.uri.path.substring(1));
+    _clientDir.serveFile(new File(fileUri.toFilePath()), request);
   }
-}
-
-Future handleClean(HttpRequest request) async {
-  final items = await queryItems();
-  final deletes = items.map((item) => item.key).toList();
-  return dbService.commit(deletes: deletes);
-}
-
-Future readJSONRequest(HttpRequest request) =>
-    request.transform(UTF8.decoder).transform(JSON.decoder).single;
-
-Future sendJSONResponse(HttpRequest request, json) {
-  request.response
-    ..headers.contentType = ContentType.JSON
-    ..headers.set("Cache-Control", "no-cache")
-    ..add(UTF8.encode(JSON.encode(json)));
-
-  return request.response.close();
 }
