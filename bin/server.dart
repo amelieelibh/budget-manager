7983b7fe226f68884ab44c97030e509c8d1ff798
Amelie Barbosa <amelieelibh@gmail.com>
2017-06-06 20:40:46 -0500

    fix at last to deploy

--- bin/server.dart
+++ bin/server.dart
@@ -1,65 +1,37 @@
-library budgetserver;
-
 import 'dart:async';
 import 'dart:io';
 
-import 'package:http_server/http_server.dart';
-import 'package:logging/logging.dart';
-import 'package:rpc/rpc.dart';
+import 'package:appengine/appengine.dart';
 
-//import 'package:server_code_lab/server/piratesapi.dart';
-
-//final ApiServer _apiServer = new ApiServer(prettyPrint: true);
-
-// Create a virtual directory used to serve our client code from
-// the 'build/web' directory.
-final String _buildPath =
-    Platform.script.resolve('../build/web/').toFilePath();
-final VirtualDirectory _clientDir = new VirtualDirectory(_buildPath);
-
-Future main() async {
-  // Add a bit of logging...
-  Logger.root..level = Level.INFO
-             ..onRecord.listen(print);
-
-  // Set up a server serving the pirate API.
-  /*
-  _apiServer.addApi(new PiratesApi());
-  */
-  HttpServer server =
-      await HttpServer.bind(InternetAddress.ANY_IP_V4, 8082);
-  server.listen(requestHandler);
-  print('Server listening on http://${server.address.host}:'
-        '${server.port}');
+void main() {
+  runAppEngine(requestHandler);
 }
 
 Future requestHandler(HttpRequest request) async {
-  if (request.uri.path.startsWith('/budget')) {
-    // Handle the API request.
-    //var apiResponse;
-    try {
-      //var apiRequest = new HttpApiRequest.fromHttpRequest(request);
-      //apiResponse =
-      //    await _apiServer.handleHttpApiRequest(apiRequest);
-    } catch (error, stack) {
-      /*var exception =
-          error is Error ? new Exception(error.toString()) : error;
-      apiResponse = new HttpApiResponse.error(
-          HttpStatus.INTERNAL_SERVER_ERROR, exception.toString(),
-          exception, stack);
-      */
-    }
-    return sendApiResponse(null/*apiResponse*/, request.response);
+  if (request.uri.path == '/budget') {
+    //handleItems(request);
   } else if (request.uri.path == '/') {
-    // Redirect to the piratebadge.html file. This will initiate
-    // loading the client application.
-    request.response.redirect(Uri.parse('/index.html'));
+    await request.response.redirect(Uri.parse('/index.html'));
   } else {
-    // Serve the requested file (path) from the virtual directory,
-    // minus the preceeding '/'. This will fail with a 404 Not Found
-    // if the request is not for a valid file.
-    var fileUri = new Uri.file(_buildPath)
-        .resolve(request.uri.path.substring(1));
-    _clientDir.serveFile(new File(fileUri.toFilePath()), request);
+    await context.assets.serve();
   }
 }
+/*
+Future handleItems(HttpRequest request) async {
+  if (request.method == 'GET') {
+    final List<Item> items = await queryItems();
+    final result = items.map((item) => item.serialize()).toList();
+    final json = {'success': true, 'result': result};
+    await sendJSONResponse(request, json);
+  } else if (request.method == 'POST') {
+    final json = await readJSONRequest(request);
+    final item = Item.deserialize(json)..parentKey = itemsRoot;
+    final error = item.validate();
+    if (error != null) {
+      await sendJSONResponse(request, {'success': false, 'error': error});
+    } else {
+      await dbService.commit(inserts: [item]);
+      await sendJSONResponse(request, {'success': true});
+    }
+  }
+}*/
